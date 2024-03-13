2232a4b12d8c843a6038df6d3aad0cb4cf522f52
SCiarella <simoneciarella@gmail.com>
2024-03-13 13:43:33 +0100

    Finalize 02.02

--- examples/coupling_functions/functions_CNODE_loss.jl
+++ examples/coupling_functions/functions_CNODE_loss.jl
@@ -2,19 +2,8 @@
 # You can use this to train the closure term to reproduce the right hand side
 function mean_squared_error(f, st, x, y, θ, λ) 
     prediction = Array(f(x, θ, st)[1])
-    #total_loss = sum(abs2, prediction - y)
-    #return total_loss, nothing
     total_loss = sum(abs2, prediction - y) / sum(abs2, y)
-#    println(θ)
     return total_loss + λ *norm(θ, 1), nothing
-    ## Add a regularization for the parameters that are weakly used
-    #reg_term = 0.0
-    #for θ_i in θ
-    #    if abs(θ_i) < 0.1
-    #        reg_term += abs(θ_i)
-    #    end
-    #end
-    #return total_loss + λ *reg_term, nothing
 end
 # the loss functions are randomized by selecting a subset of the data, because it would be too expensive to use the entire dataset at each iteration
 function create_randloss_derivative(GS_data, FG_target, f, st; nuse = size(GS_data, 2), λ=0)
@@ -31,27 +20,38 @@ end
 
 
 # auxiliary function to solve the NeuralODE, given parameters p
-function predict_u_CNODE(uv0,θ,tg_size)
+function predict_u_CNODE(uv0,θ,tg)
+    tg_size = size(tg)
     sol = Array(training_CNODE(uv0, θ, st)[1])
+    #println("sol size ", size(sol))
+    #println("tg size ", size(tg))
+    #println(sol[:,:,1] == tg[:,:,1])
     
-    # handle unstable solver
-    # //TODO you probably don't want to use this
-    if any(isnan, sol)
-        # Create a mask of the NaN values
-        nan_mask = isnan.(sol)
-        # Get the non-NaN values
-        non_nan_values = sol[.!nan_mask]
-        
-        target = non_nan_values[rand(1:length(non_nan_values), prod(tg_size))] |> f32
-        target = reshape(target, tg_size)
-        sol = target
-        
-    end
-    
-    return sol
+    ## handle unstable solver
+    #if any(isnan, sol)
+    #    # if some steps succesfully run, then use them for the loss
+    #    nok = 1
+    #    while any(isnan, sol[:, :, 1:nok])
+    #        nok += 1
+    #    end
+    #    if nok > 1
+    #        println("Unstability after ", nok, " steps")
+    #        tg = tg[:,:,1:nok]
+    #        sol = sol[:,:,1:nok]
+    #    else
+    #        # otherwise run the auxiliary solver
+    #        println("Using auxiliary solver ")
+    #        sol = Array(training_CNODE_2(uv0, θ, st)[1])
+    #        if any(isnan, sol)
+    #            println("ERROR: NaN detected in the prediction")
+    #            return fill(1e6 * sum(θ), tg_size)
+    #        end
+    #    end
+    #end
+    return sol, tg[:,:,1:size(sol,3)]
 end
 # This is the function to create the loss
-function create_randloss_MulDtO(target; nunroll, nintervals=1, nsamples, λ)
+function create_randloss_MulDtO(target; nunroll, nintervals=1, nsamples, λ_c, λ_l1)
     # Get the number of time steps 
     d = ndims(target)
     nt = size(target, d)
@@ -62,30 +62,22 @@ function create_randloss_MulDtO(target; nunroll, nintervals=1, nsamples, λ)
         # and select a certain number of samples
         trajectory = Zygote.@ignore trajectory[:,rand(1:size(trajectory,2), nsamples),:]
         # this is the loss evaluated for each multishooting set
-        loss_MulDtO_oneset(trajectory, θ, nunroll=nunroll, nintervals=nintervals, nsamples=nsamples, λ=λ)
+        loss_MulDtO_oneset(trajectory, θ, nunroll=nunroll, nintervals=nintervals, nsamples=nsamples, λ_c=λ_c, λ_l1=λ_l1)
     end
 end
 # the parameter λ sets how strongly we make the pieces match (continuity term)
-function loss_MulDtO_oneset(trajectory, θ; λ=1e1, nunroll, nintervals, nsamples=nsamples)
+function loss_MulDtO_oneset(trajectory, θ; λ_c=1e1, λ_l1=1e1, nunroll, nintervals, nsamples=nsamples)
 
     # Take all the time intervals and concatenate them in the batch dimension
     list_tr = cat([trajectory[:, :, 1+(i-1)*nunroll:1+i*nunroll] for i in 1:nintervals]..., dims=2)
     # get all the initial conditions 
     list_starts = cat([trajectory[:, :, 1+(i-1)*nunroll] for i in 1:nintervals]..., dims=2)
-    pred = predict_u_CNODE(list_starts,θ,size(list_tr))
-    # check if pred contains any nan
-    if any(isnan, pred)
-        println("ERROR: NaN detected in the prediction")
-        #pred = list_tr .+ 1e10
-        #return Inf, nothing
-        println("size tr ",size(list_tr))
-        println("size pred ", size(pred))
-        print("\n**********\n")
-    end
+    pred, list_tr = predict_u_CNODE(list_starts,θ,list_tr)
+    
     # the loss is the sum of the differences between the real trajectory and the predicted one
     loss = sum(abs2, list_tr.- pred) ./ sum(abs2, list_tr)
 
-    if λ>0
+    if λ_c>0 && size(list_tr,3)==nunroll+1
         # //TODO check if the continuity term is correct
         # Then I compute the continuity term by comparing end of one interval with the start of the next one
         pred_end = pred[:,:,end]
@@ -104,5 +96,5 @@ function loss_MulDtO_oneset(trajectory, θ; λ=1e1, nunroll, nintervals, nsample
         continuity = 0
     end
 
-    return loss+(continuity*λ), nothing
+    return loss+(continuity*λ_c) + λ_l1*norm(θ), nothing
 end
