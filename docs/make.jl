803e7a4beff8ad4e26c6c2d0eb923099b549edb7
Aron <aronpjansen@gmail.com>
2024-03-26 14:25:30 +0100

    run JuliaFormatter.format

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,22 @@
 using CoupledNODE
 using Documenter
 
-DocMeta.setdocmeta!(CoupledNODE, :DocTestSetup, :(using CoupledNODE); recursive=true)
+DocMeta.setdocmeta!(CoupledNODE, :DocTestSetup, :(using CoupledNODE); recursive = true)
 
 makedocs(;
-    modules=[CoupledNODE],
-    authors="Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>, Luisa Orozco <l.orozco@esciencecenter.nl>, Simone Ciarella <s.ciarella@esciencecenter.nl>, Aron Jansen <a.p.jansen@esciencecenter.nl>",
-    sitename="CoupledNODE.jl",
-    format=Documenter.HTML(;
-        canonical="https://DEEPDIP-project.github.io/CoupledNODE.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [CoupledNODE],
+    authors = "Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>, Luisa Orozco <l.orozco@esciencecenter.nl>, Simone Ciarella <s.ciarella@esciencecenter.nl>, Aron Jansen <a.p.jansen@esciencecenter.nl>",
+    sitename = "CoupledNODE.jl",
+    format = Documenter.HTML(;
+        canonical = "https://DEEPDIP-project.github.io/CoupledNODE.jl",
+        edit_link = "main",
+        assets = String[]
     ),
-    pages=[
-        "Home" => "index.md",
-    ],)
+    pages = [
+        "Home" => "index.md"
+    ])
 
 deploydocs(;
-    repo="github.com/DEEPDIP-project/CoupledNODE.jl",
-    devbranch="main",
+    repo = "github.com/DEEPDIP-project/CoupledNODE.jl",
+    devbranch = "main"
 )
