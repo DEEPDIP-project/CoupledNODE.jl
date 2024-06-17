fc7378735a5d497a63dbdbba2412b0ee7f8d25da
SCiarella <simoneciarella@gmail.com>
2024-06-17 12:34:54 +0200

    Add test results

--- examples/src/test_derivatives.jl
+++ examples/src/test_derivatives.jl
@@ -1,6 +1,5 @@
 const MY_TYPE = Float32 # use float32 if you plan to use a GPU
 
-
 # Definition of the initial condition as a random perturbation over a constant background to add variety. 
 import Random
 function initial_condition(U₀, V₀, ε_u, ε_v; nsimulations = 1)
@@ -22,7 +21,7 @@ k = 0.062f0;
 # Declare the grid object 
 include("./../../src/grid.jl")
 dux = duy = dvx = dvy = 1.0f0
-nux = nuy = nvx = nvy = 64
+nux = nuy = nvx = nvy = 1024
 nsim = 10
 u_initial, v_initial = initial_condition(U₀, V₀, ε_u, ε_v, nsimulations = nsim);
 grid_GS_u = make_grid(dim = 2, dtype = MY_TYPE, dx = dux, nx = nux, dy = duy,
@@ -30,7 +29,7 @@ grid_GS_u = make_grid(dim = 2, dtype = MY_TYPE, dx = dux, nx = nux, dy = duy,
 grid_GS_v = make_grid(dim = 2, dtype = MY_TYPE, dx = dvx, nx = nvx, dy = dvy,
     ny = nvy, nsim = nsim, grid_data = v_initial)
 
-    #####################   DIFFERENT LAPLACIAN IMPLEMENTATIONS   #####################
+#####################   DIFFERENT LAPLACIAN IMPLEMENTATIONS   #####################
 # OLD Laplacian
 function add_dim_1(x)
     return reshape(x, 1, size(x)...)
@@ -79,10 +78,21 @@ function Laplacian_c2(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     circshift!(umx0, u, (-1, 0, 0))
     circshift!(umy0, u, (0, -1, 0))
 
-    @. ((upx0 - 2 * u + umx0) + (upy0 - 2 * u + umy0)) / Δx2# + Δy2)
+    @. ((upx0 - 2 * u + umx0) + (upy0 - 2 * u + umy0)) / Δx2
 end
-# [!] however, this is not compatible with CUDA
 
+###########
+# this uses ShiftedArrays
+# [!] however, this is not compatible with CUDA
+using ShiftedArrays
+function Laplacian_c3(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
+    upx = ShiftedArrays.circshift(u, (1, 0, 0))
+    upy = ShiftedArrays.circshift(u, (0, 1, 0))
+    umx = ShiftedArrays.circshift(u, (-1, 0, 0))
+    umy = ShiftedArrays.circshift(u, (0, -1, 0))
+
+    @. (upx + umx + upy + umy - u * 4) / Δx2
+end
 
 ###########
 # this uses a fork from "https://github.com/RainerHeintzmann/ShiftedArrays.jl.git"
@@ -93,68 +103,59 @@ end
 #Pkg.add(url="https://github.com/DEEPDIP-project/cShiftedArrays.jl.git")
 #Pkg.add(url="https://github.com/JuliaArrays/ShiftedArrays.jl.git")
 using cShiftedArrays
-function Laplacian_c3(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
+function Laplacian_c4(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     upx = collect(cShiftedArrays.circshift(u, (1, 0, 0)))
     upy = collect(cShiftedArrays.circshift(u, (0, 1, 0)))
     umx = collect(cShiftedArrays.circshift(u, (-1, 0, 0)))
     umy = collect(cShiftedArrays.circshift(u, (0, -1, 0)))
 
-    #@. (upx + umx + upy + umy - u * 4) / (Δx2 + Δy2)
     @. (upx + umx + upy + umy - u * 4) / Δx2# + Δy2)
 end
-
 
 ###############
 # This usese the Lux library and a convolutional layer
 using Lux, Flux
 using MLUtils
 rng = Random.seed!(1234);
-M = Lux.Chain( 
+M = Lux.Chain(
     # Pad for PBC
     x -> begin
-        @views uu2[2:end-1, 2:end-1, 1, :] .= x
-        @views uu2[1, 2:end-1, 1, :] .= x[end, :,:]
-        @views uu2[end, 2:end-1, 1, :] .= x[1, :, :]
-        @views uu2[2:end-1, 1, 1, :] .= x[:, end, :]
-        @views uu2[2:end-1, end, 1,:] .= x[:, 1, :]
+        @views uu2[2:(end - 1), 2:(end - 1), 1, :] .= x
+        @views uu2[1, 2:(end - 1), 1, :] .= x[end, :, :]
+        @views uu2[end, 2:(end - 1), 1, :] .= x[1, :, :]
+        @views uu2[2:(end - 1), 1, 1, :] .= x[:, end, :]
+        @views uu2[2:(end - 1), end, 1, :] .= x[:, 1, :]
         uu2
     end,
-    Lux.Conv((3,3),1=>1, use_bias=false),
-    SelectDim(3, 1),
+    Lux.Conv((3, 3), 1 => 1, use_bias = false),
+    SelectDim(3, 1)
 )
 θ, st = Lux.setup(rng, M);
 using ComponentArrays
 θ = ComponentArrays.ComponentArray(θ)
-θ.layer_2.weight = [
-    0.0f0 1.0f0 0.0f0;
-    1.0f0 -4.0f0 1.0f0;
-    0.0f0 1.0f0 0.0f0
-]
+θ.layer_2.weight = [0.0f0 1.0f0 0.0f0;
+                    1.0f0 -4.0f0 1.0f0;
+                    0.0f0 1.0f0 0.0f0]
 function Laplacian_Lux(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     Lux.apply(M, u, θ, st)[1]
 end
 
-
 ###############
 # this is a Lux variant with padding instead of preallocation
-Mpd = Lux.Chain( 
+Mpd = Lux.Chain(
     # Pad for PBC
-    WrappedFunction(x -> unsqueeze(NNlib.pad_circular(x, 1; dims=[1,2]), 3)), 
-    Lux.Conv((3,3),1=>1, use_bias=false),
-    SelectDim(3, 1),
+    WrappedFunction(x -> unsqueeze(NNlib.pad_circular(x, 1; dims = [1, 2]), 3)),
+    Lux.Conv((3, 3), 1 => 1, use_bias = false),
+    SelectDim(3, 1)
 )
 θpd, stpd = Lux.setup(rng, Mpd);
 θpd = ComponentArrays.ComponentArray(θ)
-θpd.layer_2.weight = [
-    0.0f0 1.0f0 0.0f0;
-    1.0f0 -4.0f0 1.0f0;
-    0.0f0 1.0f0 0.0f0
-]
+θpd.layer_2.weight = [0.0f0 1.0f0 0.0f0;
+                      1.0f0 -4.0f0 1.0f0;
+                      0.0f0 1.0f0 0.0f0]
 function Laplacian_Lux_pd(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     Lux.apply(Mpd, u, θpd, stpd)[1]
 end
-
-
 
 ###############
 # This one uses a stencil and matrix multiplication
@@ -167,31 +168,26 @@ K = Array(Tridiagonal([1.0f0 for i in 1:(N - 1)], [-2.0f0 for i in 1:N],
 K[end, 1] = 1.0
 K[1, end] = 1.0
 function Laplacian_stencil(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
-    NNlib.batched_mul(K,u_initial) + NNlib.batched_mul(u_initial,K)
+    NNlib.batched_mul(K, u_initial) + NNlib.batched_mul(u_initial, K)
 end
-
 
 ###############
 # This one uses convolution
 FT = MY_TYPE
 n = nux
-D = [
-    0.0f0 1.0f0 0.0f0;
-    1.0f0 -4.0f0 1.0f0;
-    0.0f0 1.0f0 0.0f0
-]
+D = [0.0f0 1.0f0 0.0f0;
+     1.0f0 -4.0f0 1.0f0;
+     0.0f0 1.0f0 0.0f0]
 D_3D = reshape(D, (3, 3, 1, 1))
 function Laplacian_conv(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     # Create the ghost grid
-    uu[2:end-1, 2:end-1, :] .= u
-    uu[1, 2:end-1,:] .= u[end, :,:]
-    uu[end, 2:end-1,:] .= u[1, :, :]
-    uu[2:end-1, 1, :] .= u[:, end, :]
-    uu[2:end-1, end, :] .= u[:, 1, :]
-    dropdims(NNlib.conv(unsqueeze(uu, 3), D_3D), dims=3)
+    uu[2:(end - 1), 2:(end - 1), :] .= u
+    uu[1, 2:(end - 1), :] .= u[end, :, :]
+    uu[end, 2:(end - 1), :] .= u[1, :, :]
+    uu[2:(end - 1), 1, :] .= u[:, end, :]
+    uu[2:(end - 1), end, :] .= u[:, 1, :]
+    dropdims(NNlib.conv(unsqueeze(uu, 3), D_3D), dims = 3)
 end
-
-
 
 #############
 
@@ -206,8 +202,8 @@ upx0 = similar(u_initial);
 umx0 = similar(u_initial);
 upy0 = similar(u_initial);
 umy0 = similar(u_initial);
-uu = zeros(MY_TYPE,size(u_initial)[1]+2, size(u_initial)[2]+2, size(u_initial)[3]);
-uu2 = zeros(MY_TYPE,size(u_initial)[1]+2, size(u_initial)[2]+2, 1, size(u_initial)[3]);
+uu = zeros(MY_TYPE, size(u_initial)[1] + 2, size(u_initial)[2] + 2, size(u_initial)[3]);
+uu2 = zeros(MY_TYPE, size(u_initial)[1] + 2, size(u_initial)[2] + 2, 1, size(u_initial)[3]);
 
 # Precompile all the functions
 function_list = [
@@ -215,35 +211,78 @@ function_list = [
     Laplacian_c1,
     Laplacian_c2,
     Laplacian_c3,
+    Laplacian_c4,
     Laplacian_Lux,
     Laplacian_Lux_pd,
     Laplacian_stencil,
-    Laplacian_conv,
+    Laplacian_conv
 ]
 for f in function_list
     println(f)
     f(grid_GS_u.grid_data, grid_GS_u.dx, grid_GS_u.dy)
 end
 
-# and test them by running them 10 times
-GC.gc()
-for f in function_list
-    println(f)
-    @time for i in 1:10
-        f(grid_GS_u.grid_data, grid_GS_u.dx, grid_GS_u.dy)
-    end
-    GC.gc()
+# Check if they produce the same output
+results = [method(grid_GS_u.grid_data, grid_GS_u.dx, grid_GS_u.dy)
+           for method in function_list];
+if all(results[1] ≈ result for result in results[2:end])
+    println("All methods produce the same results")
+else
+    println("ERROR: Some methods produce different results")
 end
 
+# and test them by running them 10 times
+using Statistics
+results = Dict()
+GC.gc()
+for f in function_list
+    times = Float64[]
+    allocations = Int64[]
+    gctime = Float64[]
+    for i in 1:10
+        result = @timed f(grid_GS_u.grid_data, grid_GS_u.dx, grid_GS_u.dy)
+        push!(times, result[2])
+        push!(allocations, result[3])
+        push!(gctime, result[4])
+    end
+    GC.gc()
+    results[string(f)] = (sum(times), sum(allocations), sum(gctime))
+end
 
-# check the results
-results = [method(grid_GS_u.grid_data, grid_GS_u.dx, grid_GS_u.dy) for method in function_list];
-all(results[1] ≈ result for result in results[2:end])
+# sort the results by time
+methods = []
+times = []
+allocations = []
+gctimes = []
+println("Results sorted by time")
+for key in sort(collect(keys(results)), by = x -> results[x][1])
+    push!(methods, key)
+    push!(times, results[key][1])
+    push!(allocations, results[key][2])
+    push!(gctimes, results[key][3])
+end
 
+# And plot them
+using Plots
+p1 = bar(methods, times, title = "Execution Times", ylabel = "Time (s)",
+    yaxis = :log, legend = false, xrotation = 45)
+p2 = bar(methods, allocations, title = "Memory Allocations",
+    xlabel = "Methods", ylabel = "Bytes", legend = false, xrotation = 45)
+p3 = bar(methods, gctimes, title = "GC Time", xlabel = "Methods",
+    ylabel = "Time (s)", legend = false, xrotation = 45)
 
+P = plot(p1,
+    p2,
+    p3,
+    layout = (3, 1),
+    size = (600, 900),
+    suptitle = "gridsize = $(size(u_initial)[1])x$(size(u_initial)[2])x$(size(u_initial)[3])")
+savefig(P,
+    "examples/plots/derivatives_CPUbenchmark_$(size(u_initial)[1])_$(size(u_initial)[2])_$(size(u_initial)[3]).png")
 
 ##############################
 ##############################
+########## GPU  ##############
 ##############################
 ##############################
 # If you are on a GPU, you can also test the GPU version of the Laplacian
@@ -265,62 +304,59 @@ upx0 = cu(similar(u_initial));
 umx0 = cu(similar(u_initial));
 upy0 = cu(similar(u_initial));
 umy0 = cu(similar(u_initial));
-uu = cu(zeros(MY_TYPE,size(u_initial)[1]+2, size(u_initial)[2]+2, size(u_initial)[3]));
-uu2 = cu(zeros(MY_TYPE,size(u_initial)[1]+2, size(u_initial)[2]+2, 1, size(u_initial)[3]));
+uu = cu(zeros(MY_TYPE, size(u_initial)[1] + 2, size(u_initial)[2] + 2, size(u_initial)[3]));
+uu2 = cu(zeros(
+    MY_TYPE, size(u_initial)[1] + 2, size(u_initial)[2] + 2, 1, size(u_initial)[3]));
 K = cu(K)
 D_3D = cu(D_3D)
-M = Lux.Chain( 
+M = Lux.Chain(
     # Pad for PBC
     x -> begin
-        @views uu2[2:end-1, 2:end-1, 1, :] .= x
-        @views uu2[1, 2:end-1, 1, :] .= x[end, :,:]
-        @views uu2[end, 2:end-1, 1, :] .= x[1, :, :]
-        @views uu2[2:end-1, 1, 1, :] .= x[:, end, :]
-        @views uu2[2:end-1, end, 1,:] .= x[:, 1, :]
+        @views uu2[2:(end - 1), 2:(end - 1), 1, :] .= x
+        @views uu2[1, 2:(end - 1), 1, :] .= x[end, :, :]
+        @views uu2[end, 2:(end - 1), 1, :] .= x[1, :, :]
+        @views uu2[2:(end - 1), 1, 1, :] .= x[:, end, :]
+        @views uu2[2:(end - 1), end, 1, :] .= x[:, 1, :]
         uu2
     end,
-    Lux.Conv((3,3),1=>1, use_bias=false),
-    SelectDim(3, 1),
+    Lux.Conv((3, 3), 1 => 1, use_bias = false),
+    SelectDim(3, 1)
 )
 θ, st = setup(rng, M);
 θ = ComponentArrays.ComponentArray(θ)
-θ.layer_2.weight = [
-    0.0f0 1.0f0 0.0f0;
-    1.0f0 -4.0f0 1.0f0;
-    0.0f0 1.0f0 0.0f0
-]
+θ.layer_2.weight = [0.0f0 1.0f0 0.0f0;
+                    1.0f0 -4.0f0 1.0f0;
+                    0.0f0 1.0f0 0.0f0]
 function Laplacian_Lux(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     Lux.apply(M, u, θ, st)[1]
 end
 # this is a Lux variant with padding instead of preallocation
-Mpd = Lux.Chain( 
+Mpd = Lux.Chain(
     # Pad for PBC
-    WrappedFunction(x -> unsqueeze(NNlib.pad_circular(x, 1; dims=[1,2]), 3)), 
+    WrappedFunction(x -> unsqueeze(NNlib.pad_circular(x, 1; dims = [1, 2]), 3)),
     #x -> reshape(x, size(x)[1], size(x)[2], 1, size(x)[3]),
-    Lux.Conv((3,3),1=>1, use_bias=false),
-    SelectDim(3, 1),
+    Lux.Conv((3, 3), 1 => 1, use_bias = false),
+    SelectDim(3, 1)
 )
 θpd, stpd = Lux.setup(rng, Mpd);
 θpd = ComponentArrays.ComponentArray(θ)
-θpd.layer_2.weight = [
-    0.0f0 1.0f0 0.0f0;
-    1.0f0 -4.0f0 1.0f0;
-    0.0f0 1.0f0 0.0f0
-]
+θpd.layer_2.weight = [0.0f0 1.0f0 0.0f0;
+                      1.0f0 -4.0f0 1.0f0;
+                      0.0f0 1.0f0 0.0f0]
 function Laplacian_Lux_pd(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     Lux.apply(Mpd, u, θpd, stpd)[1]
 end
 function Laplacian_stencil(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
-    NNlib.batched_mul(K,u_initial) + NNlib.batched_mul(u_initial,K)
+    NNlib.batched_mul(K, u_initial) + NNlib.batched_mul(u_initial, K)
 end
 function Laplacian_conv(u, Δx2, Δy2 = 0.0, Δz2 = 0.0)
     # Create the ghost grid
-    uu[2:end-1, 2:end-1, :] .= u
-    uu[1, 2:end-1,:] .= u[end, :,:]
-    uu[end, 2:end-1,:] .= u[1, :, :]
-    uu[2:end-1, 1, :] .= u[:, end, :]
-    uu[2:end-1, end, :] .= u[:, 1, :]
-    dropdims(NNlib.conv(unsqueeze(uu, 3), D_3D), dims=3)
+    uu[2:(end - 1), 2:(end - 1), :] .= u
+    uu[1, 2:(end - 1), :] .= u[end, :, :]
+    uu[end, 2:(end - 1), :] .= u[1, :, :]
+    uu[2:(end - 1), 1, :] .= u[:, end, :]
+    uu[2:(end - 1), end, :] .= u[:, 1, :]
+    dropdims(NNlib.conv(unsqueeze(uu, 3), D_3D), dims = 3)
 end
 
 Laplacian_Lux(cugu.grid_data, cugu.dx, cugu.dy);
@@ -333,8 +369,8 @@ for f in function_list
     println("\n")
     println(f)
     try
-        f(cugu.grid_data, cugu.dx, cugu.dy);
-        push!(f_good,f)
+        f(cugu.grid_data, cugu.dx, cugu.dy)
+        push!(f_good, f)
         GC.gc()
         CUDA.reclaim()
         CUDA.memory_status()
