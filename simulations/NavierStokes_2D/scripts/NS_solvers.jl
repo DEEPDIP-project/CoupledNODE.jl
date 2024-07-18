a2aee1302bdf30035f83c5ee1cec3141e316339e
Luisa Orozco <l.orozco@esciencecenter.nl>
2024-07-18 14:20:51 +0200

    Improve visualization NS_solvers

--- simulations/NavierStokes_2D/scripts/NS_solvers.jl
+++ simulations/NavierStokes_2D/scripts/NS_solvers.jl
@@ -43,6 +43,7 @@ saveat = 20 * dt;
         log = INS.timelogger(; nupdate = 100)
     )
 );
+# Look at the generated animation of the evolution of the vorticity field in time in plots/vorticity_INS.mkv.
 # TODO: can we store history of u for comparisons? probably not
 
 # ## SciML
@@ -141,27 +142,28 @@ Plots.plot(p1, p2, p3, layout = (3, 1), size = (600, 800))
 
 # ### Plots: final state of $u$
 using Plots
-p1 = Plots.heatmap(title = "u in SciML ODE", sol_ode.u[end][:, :, 1]);
-p2 = Plots.heatmap(title = "u in SciML CNODE", sol_node.u[end][:, :, 1]);
-p3 = Plots.heatmap(title = "u in INS", state.u[1]);
-p4 = Plots.heatmap(title = "u_INS-u_ODE", state.u[1] - sol_ode.u[end][:, :, 1]);
-p5 = Plots.heatmap(title = "u_INS-u_CNODE", state.u[1] - sol_node.u[end][:, :, 1]);
+p1 = Plots.heatmap(title = "\$u\$ SciML ODE", sol_ode.u[end][:, :, 1], ticks = false);
+p2 = Plots.heatmap(title = "\$u\$ SciML CNODE", sol_node.u[end][:, :, 1], ticks = false);
+p3 = Plots.heatmap(title = "\$u\$ INS", state.u[1], ticks = false);
+p4 = Plots.heatmap(title = "\$u_{INS} - u_{ODE}\$", state.u[1] - sol_ode.u[end][:, :, 1], ticks = false);
+p5 = Plots.heatmap(title = "\$u_{INS}-u_{CNODE}\$", state.u[1] - sol_node.u[end][:, :, 1], ticks = false);
 p6 = Plots.heatmap(
-    title = "u_CNODE-u_ODE", sol_node.u[end][:, :, 1] - sol_ode.u[end][:, :, 1]);
-Plots.plot(p1, p2, p3, p4, p5, p6, layout = (2, 3), size = (900, 600))
+    title = "\$u_{CNODE}-u_{ODE}\$", sol_node.u[end][:, :, 1] - sol_ode.u[end][:, :, 1], ticks = false);
+Plots.plot(p1, p2, p3, p4, p5, p6, layout = (2, 3), size = (900, 600), ticks = false)
 
 # ### Plots: vorticity
 vins = INS.vorticity((state.u[1], state.u[2]), setup)
 vode = INS.vorticity((sol_ode.u[end][:, :, 1], sol_ode.u[end][:, :, 2]), setup)
 vnode = INS.vorticity((sol_node.u[end][:, :, 1], sol_node.u[end][:, :, 2]), setup)
+vor_lims = (-5, 5)
+diff_lims = (-0.4, 0.4)
 
-Plots.heatmap(title = "vorticity INS-ODE", -)
-p1 = Plots.heatmap(title = "vorticity in SciML ODE", vode);
-p2 = Plots.heatmap(title = "vorticity in SciML CNODE", vnode);
-p3 = Plots.heatmap(title = "vorticity in INS", vins);
-p4 = Plots.heatmap(title = "INS-ODE", vins - vode);
-p5 = Plots.heatmap(title = "INS-CNODE", vins - vnode);
-p6 = Plots.heatmap(title = "CNODE-ODE", vode - vnode);
+p1 = Plots.heatmap(title = "\$\\omega\$ SciML ODE", vode, color = :viridis, ticks = false, clims = vor_lims);
+p2 = Plots.heatmap(title = "\$\\omega\$ in SciML CNODE", vnode, color = :viridis, ticks = false, clims = vor_lims);
+p3 = Plots.heatmap(title = "vorticity \$(\\omega)\$ in INS", vins, color = :viridis, ticks = false, clims = vor_lims);
+p4 = Plots.heatmap(title = "\$\\omega_{INS}-\\omega_{ODE}\$", vins - vode, clim=diff_lims, ticks = false);
+p5 = Plots.heatmap(title = "\$\\omega_{INS}-\\omega_{CNODE}\$", vins - vnode, clim=diff_lims, ticks = false);
+p6 = Plots.heatmap(title = "\$\\omega_{CNODE}-\\omega_{ODE}\$", vode - vnode, ticks = false, clim=(0, 0.2));
 Plots.plot(p1, p2, p3, p4, p5, p6, layout = (2, 3), size = (900, 600))
 
 # ### Animations
@@ -173,7 +175,7 @@ let
     #obs = Observable(sol.u[1][Iu[i], i])
     #ω = IncompressibleNavierStokes.vorticity((u[:, :, 1], u[:, :, 2]), setup)
     obs = Observable(INS.vorticity((sol_ode.u[1][:, :, 1], sol_ode.u[1][:, :, 2]), setup))
-    fig = GLMakie.heatmap(obs)
+    fig = GLMakie.heatmap(obs, colorrange=vor_lims)
     fig |> display
     for u in sol_ode.u
         #obs[] = u[Iu[i], i]
@@ -189,7 +191,7 @@ anim = Animation()
 for (idx, (t, u)) in enumerate(zip(sol_ode.t, sol_ode.u))
     ω = INS.vorticity((u[:, :, 1], u[:, :, 2]), setup)
     title = @sprintf("Vorticity SciML, t = %.3f s", t)
-    fig = Plots.heatmap(ω'; xlabel = "x", ylabel = "y", title)
+    fig = Plots.heatmap(ω'; xlabel = "x", ylabel = "y", title, clims = vor_lims, color = :viridis)
     frame(anim, fig)
 end
 gif(anim, "simulations/NavierStokes_2D/plots/vorticity_SciML.gif", fps = 15)
