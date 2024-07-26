775f2bb380e09c287a47416fa6c94971e4f9d963
SCiarella <simoneciarella@gmail.com>
2024-07-26 16:46:56 +0200

    Fully differentiate INS using Enzyme

--- simulations/NavierStokes_2D/scripts/NS_closure.jl
+++ simulations/NavierStokes_2D/scripts/NS_closure.jl
@@ -3,7 +3,6 @@ using IncompressibleNavierStokes
 # [!] you can not use the abbreviation 'INS' with Enzyme, because it does not know how to differentiate the getname function!
 INS = IncompressibleNavierStokes
 
-# [https://github.com/JuliaMath/AbstractFFTs.jl/issues/99] we cannot define reverse-mode rules for mul!(y::AbstractArray, plan::Plan, x::AbstractArray), since we don't know what the plan is and therefore don't know how to normalize.
 
 
 # Setup and initial condition
@@ -16,21 +15,30 @@ x , y = LinRange(lims..., n + 1), LinRange(lims..., n + 1)
 setup = Setup(x, y; Re, ArrayType);
 ustart = random_field(setup, T(0));
 psolver = psolver_spectral(setup);
+# [!] can not use spectral solver with Enzyme
+# [https://github.com/JuliaMath/AbstractFFTs.jl/issues/99] we cannot define reverse-mode rules for mul!(y::AbstractArray, plan::Plan, x::AbstractArray), since we don't know what the plan is and therefore don't know how to normalize.
+# The reason underneath is connected to Complex numbers as explained in Enzyme FAQ.
+# one could do this to ignore the poisson solver
+#EnzymeRules.inactive(::typeof(IncompressibleNavierStokes.poisson!), args...) = nothing
+# but this is a cheat that breaks completely a posteriori fitting.
+# Alternatively, you can use different poisson solvers
+psolver = IncompressibleNavierStokes.psolver_direct(setup)
+cache_p = INS.pressure(ustart, nothing, 0.0f0, setup; psolver)
+
+
 dt = T(1e-3)
 trange = (T(0), T(1))
 savevery = 20
 saveat = 20 * dt
-#trange = (T(0), T(10*dt))
-#savevery = 1
-#saveat = dt
 
-# Syver's solver 
-# we know that mathematically it is the best one 
+
+# Solving using INS semi-implicit method
 (state, outputs), time_ins, allocation, gc, memory_counters = @timed solve_unsteady(;
     setup,
     ustart,
     tlims = trange,
     Δt = dt,
+    psolver = psolver,
     processors = (
         ehist = realtimeplotter(;
             setup,
@@ -50,17 +58,128 @@ using DifferentialEquations
 
 # Projected force for SciML, to use in CNODE
 F = similar(stack(ustart))
+# and prepare a cache for the force
 cache_F = (F[:,:,1], F[:,:,2])
 cache_div = INS.divergence(ustart,setup)
 cache_p = INS.pressure(ustart, nothing, 0.0f0, setup; psolver)
 cache_out = similar(F)
 Ω = setup.grid.Ω
 
-# [!] I have marked the poisson solver as inactive, because the Fourier transform is not supported by Enzyme.
-using Enzyme
-EnzymeRules.inactive(::typeof(IncompressibleNavierStokes.poisson!), args...) = nothing
 
-# * There is also an in-place version that takes the cache as a parameter p 
+# We need to modify the solver in place in order to pass everything to Enzyme
+using SparseArrays, LinearAlgebra
+struct Offset{D} end
+@inline (::Offset{D})(α) where {D} = CartesianIndex(ntuple(β -> β == α ? 1 : 0, D))
+function laplacian_mat(setup)
+    (; grid, boundary_conditions) = setup
+    (; dimension, x, N, Np, Ip, Δ, Δu, Ω) = grid
+    T = eltype(x[1])
+    D = dimension()
+    e = Offset{D}()
+    Ia = first(Ip)
+    Ib = last(Ip)
+    I = similar(x[1], CartesianIndex{D}, 0)
+    J = similar(x[1], CartesianIndex{D}, 0)
+    val = similar(x[1], 0)
+    I0 = Ia - oneunit(Ia)
+    for α = 1:D
+        a, b = boundary_conditions[α]
+        i = Ip[ntuple(β -> α == β ? (2:Np[α]-1) : (:), D)...][:]
+        ia = Ip[ntuple(β -> α == β ? (1:1) : (:), D)...][:]
+        ib = Ip[ntuple(β -> α == β ? (Np[α]:Np[α]) : (:), D)...][:]
+        for (aa, bb, j) in [(a, nothing, ia), (nothing, nothing, i), (nothing, b, ib)]
+            vala = @.(Ω[j] / Δ[α][getindex.(j, α)] / Δu[α][getindex.(j, α)-1])
+            if isnothing(aa)
+                J = [J; j .- [e(α)]; j]
+                I = [I; j; j]
+                val = [val; vala; -vala]
+            elseif aa isa PressureBC
+                J = [J; j]
+                I = [I; j]
+                val = [val; -vala]
+            elseif aa isa PeriodicBC
+                J = [J; ib; j]
+                I = [I; j; j]
+                val = [val; vala; -vala]
+            elseif aa isa SymmetricBC
+                J = [J; ia; j]
+                I = [I; j; j]
+                val = [val; vala; -vala]
+            elseif aa isa DirichletBC
+            end
+
+            valb = @.(Ω[j] / Δ[α][getindex.(j, α)] / Δu[α][getindex.(j, α)])
+            if isnothing(bb)
+                J = [J; j; j .+ [e(α)]]
+                I = [I; j; j]
+                val = [val; -valb; valb]
+            elseif bb isa PressureBC
+                J = [J; j]
+                I = [I; j]
+                val = [val; -valb]
+            elseif bb isa PeriodicBC
+                J = [J; j; ia]
+                I = [I; j; j]
+                val = [val; -valb; valb]
+            elseif bb isa SymmetricBC
+                J = [J; j; ib]
+                I = [I; j; j]
+                val = [val; -valb; valb]
+            elseif bb isa DirichletBC
+            end
+        end
+    end
+    I = Array(I)
+    J = Array(J)
+    I = I .- [I0]
+    J = J .- [I0]
+    linear = LinearIndices(Ip)
+    I = linear[I]
+    J = linear[J]
+
+    L = sparse(I, J, Array(val))
+    L
+end
+function my_cache_psolver(::Array, setup)
+    (; grid, boundary_conditions) = setup
+    (; x, Np, Ip) = grid
+    T = eltype(x[1])
+    L = laplacian_mat(setup)
+    isdefinite =
+        any(bc -> bc[1] isa PressureBC || bc[2] isa PressureBC, boundary_conditions)
+    if isdefinite
+        println("Definite")
+        # No extra DOF
+        T = Float64 # This is currently required for SuiteSparse LU
+        ftemp = zeros(T, prod(Np))
+        ptemp = zeros(T, prod(Np))
+        viewrange = (:)
+        fact = factorize(L)
+    else
+        println("Indefinite")
+        # With extra DOF
+        ftemp = zeros(T, prod(Np) + 1)
+        ptemp = zeros(T, prod(Np) + 1)
+        e = ones(T, size(L, 2))
+        L = [L e; e' 0]
+        maximum(L - L') < sqrt(eps(T)) || error("Matrix not symmetric")
+        L = @. (L + L') / 2
+        viewrange = 1:prod(Np)
+        fact = ldlt(L)
+    end
+    return ftemp, ptemp, fact, viewrange, Ip
+end
+function my_psolve!(p, f, ftemp, ptemp, fact, viewrange, Ip)
+    copyto!(view(ftemp, viewrange), view(view(f, Ip), :))
+    #ftemp[viewrange] .= vec(f[Ip])
+    ptemp .= fact \ ftemp
+    copyto!(view(view(p, Ip), :), eltype(p).(view(ptemp, viewrange)))
+    p
+end
+# get the cache for the poisson solver
+cache_ftemp, cache_ptemp, cache_fact, cache_viewrange, cache_Ip = my_cache_psolver(setup.grid.x[1], setup)
+
+# * Force in place
 F_ip(du, u, p, t)= begin
     u_view = eachslice(u; dims = 3)
     IncompressibleNavierStokes.apply_bc_u!(u_view, t, setup)
@@ -71,7 +190,8 @@ F_ip(du, u, p, t)= begin
     IncompressibleNavierStokes.divergence!(p[2], p[1], setup)
     @. p[2] *= Ω
     # Solve the Poisson equation
-    IncompressibleNavierStokes.poisson!(p[4], p[3], p[2])
+    #IncompressibleNavierStokes.poisson!(p[4], p[3], p[2])
+    my_psolve!(p[3], p[2], p[4], p[5], p[6], p[7], p[8])
     IncompressibleNavierStokes.apply_bc_p!(p[3], T(0), setup)
     # Apply pressure correction term
     IncompressibleNavierStokes.applypressure!(p[1], p[3], setup)
@@ -83,15 +203,8 @@ F_ip(du, u, p, t)= begin
     nothing
 end
 temp = similar(stack(ustart))
-myfull_cache = (cache_F,cache_div, cache_p, psolver)
+myfull_cache = (cache_F,cache_div, cache_p, cache_ftemp, cache_ptemp, cache_fact, cache_viewrange, cache_Ip)
 F_ip(temp, stack(ustart), myfull_cache, 0.0f0);
-using Plots
-Plots.heatmap(myfull_cache[1][1] - ustart[1])
-
-#struct Offset{D} end
-#@inline (::Offset{D})(α) where {D} = CartesianIndex(ntuple(β -> β == α ? 1 : 0, D))
-
-
 
 
 # Solve the ODE using ODEProblem
@@ -104,132 +217,33 @@ sol_ode, time_ode, allocation_ode, gc_ode, memory_counters_ode = @timed solve(
     saveat = saveat,
 );
 
-# I would like to train the ML using in-place updates that can maybe work with Enzyme. However the code below throws an error due to some mutation. 
-# For this reason I am trying to use the `F_syv` function that does not mutate the input. However this requires a different NN since the in place operation works with tuples
 
 import Random, Lux;
 Random.seed!(123);
 rng = Random.default_rng();
 
-# Test 1:
-# out-of-place update
+
 dummy_NN = Lux.Chain(
-#    u -> u.*3,
     Lux.Scale((1,1)),
 )
 θ_node, st_node = Lux.setup(rng, dummy_NN)
-θ_node.weight .= 1
-θ_node.bias.= 0
 θ_node
 using ComponentArrays
 Lux.apply(dummy_NN, stack(ustart), ComponentArray(θ_node), st_node)[1]
 
-# We do not need to use the Skipblock
-#Fnode_op = Lux.Chain(
-#    Lux.SkipConnection(
-#        dummy_NN,
-#        (f_NN, uv) -> begin
-#            #F_syv(uv, nothing, 0) .+ f_NN
-#            F_syv(uv, nothing, 0) #.+ f_NN
-#        end; name = "Closure"),
-#)
-#Fnode_op = Lux.Chain(dummy_NN)
-#θ_node, st_node = Lux.setup(rng, Fnode_op)
-# dudt_out(u::Array{Float32}, θ::ComponentArray{Float32}, t::Float32) = Lux.apply(Fnode_op, u, θ, st_node)[1]
-
-# Also we do not need the out-of-place version
-#create_rhs_full_oop(setup, psolver) = function rhs(u::Array{Float32}, p::Any, t::Float32)
-#    u = eachslice(u; dims = ndims(u))
-#    u = (u...,)
-#    u = IncompressibleNavierStokes.apply_bc_u(u, t, setup)
-#    F = INS.momentum(u, nothing, t, setup)
-#    F = INS.apply_bc_u(F, t, setup; dudt = true)
-#    PF = INS.project(F, setup; psolver)
-#    stack(PF)
-#end
-#F_syv = create_rhs_full_oop(setup, psolver)
-#dudt_out(u::Array{Float32}, θ::ComponentArray{Float32}, t::Float32) = F_syv(u, nothing, t) .+ Lux.apply(dummy_NN, u, θ, st_node)[1]
-#dudt_out(stack(ustart), ComponentArray(θ_node), 0.0f0)
-#prob_node = ODEProblem{false}(dudt_out, stack(ustart), trange)
-
-## This is the in-place version
-#dudt_out(du::Array{Float32}, u::Array{Float32}, θ::ComponentArray{Float32}, t::Float32) = begin
-#    F_ip(du, u, nothing, t) 
-#    gg = Lux.apply(dummy_NN, u, θ, st_node)[1]
-#    @. du .= du .+ gg
-#end
-#dudt_out(temp, stack(ustart), ComponentArray(θ_node), 0.0f0)
-#prob_node = ODEProblem{true}(dudt_out, stack(ustart), trange)
-
-# Alternative in-place version
-dudt_out(du::Array{Float32}, u::Array{Float32}, P, t::Float32) = begin
+# Force+NN in-place version
+dudt_nn(du::Array{Float32}, u::Array{Float32}, P, t::Float32) = begin
     F_ip(du, u, P[1], t) 
     gg = Lux.apply(dummy_NN, u, P[2], st_node)[1]
     @. du .= du .+ gg
 end
-P1 = (cache_F,cache_div, cache_p, psolver)
-dudt_out(temp, stack(ustart), (P1,ComponentArray(θ_node)), 0.0f0)
-prob_node = ODEProblem{true}(dudt_out, stack(ustart), trange, p=(P1,ComponentArray(θ_node)))
-prob_node = ODEProblem(dudt_out, stack(ustart), trange, (P1,ComponentArray(θ_node)))
+P1 = myfull_cache
+dudt_nn(temp, stack(ustart), (P1,ComponentArray(θ_node)), 0.0f0)
+prob_node = ODEProblem{true}(dudt_nn, stack(ustart), trange, p=(P1,ComponentArray(θ_node)))
 
 u0stacked = stack(ustart)
 sol_node, time_node, allocation_node, gc_node, memory_counters_node = @timed solve(prob_node, RK4(), u0 = u0stacked, p = (P1,ComponentArray(θ_node)), saveat = saveat, dt=dt);
 
-### Test 2:
-### in-place update 
-### Alternatively, we can follow this example to get a node using ODEProblem and in-place updates
-### https://docs.sciml.ai/SciMLSensitivity/stable/examples/neural_ode/neural_ode_flux/
-##
-### for in place updates, we need to capture the `du` in the Lux layer
-### so there is this function to create the Fnode with `du` captured
-##function make_Fnode(du, t)
-##    Fnode = Lux.Chain(
-##        Lux.SkipConnection(
-##            dummy_NN,
-##            (f_NN, uv) -> begin
-##                F_ip(du, uv, nothing, t)
-##                @. du .= du .+ f_NN
-##            end; name = "Closure"),
-##    )
-###    return Fnode
-##    return Lux.Chain(dummy_NN,
-##    )
-##end
-##
-### This alternative instead is 
-### Adjust dudt to use the modified Fnode that captures `du`
-##function dudt2(du, u, θ, t)
-##    # Create Fnode with `du` captured
-##    Fnode_with_du = make_Fnode(du, t)
-##    θ_node, st_node = Lux.setup(rng, Fnode_with_du)
-##    return Lux.apply(Fnode_with_du, u, θ, st_node)
-##end
-##
-### Define a higher-order function to initialize Fnode_with_du and setup only once
-##function create_dudt_function(du, t)
-##    # Initialize Fnode_with_du and setup only once
-##    Fnode_with_du = make_Fnode(du, t)
-##    θ_node, st_node = Lux.setup(rng, Fnode_with_du)
-##    
-##    # Return a new function that captures the initialized state and only calls Lux.apply
-##    return (du, u, θ, t) -> Lux.apply(Fnode_with_du, u, θ, st_node)
-##end
-##
-### Example usage:
-### Initialize dudt with the necessary state
-##dudt = create_dudt_function(temp, 0)
-### obtain a copy of the initial state
-##Fnode_with_du = make_Fnode(temp, 0)
-##θ_node, st_node = Lux.setup(rng, Fnode_with_du)
-##
-##dudt(temp, stack(ustart), θ_node, 0.0f0);
-##
-##
-##
-##prob_node = ODEProblem{true}(dudt, stack(ustart), trange)
-##prob_node = ODEProblem{true}(dudt2, stack(ustart), trange)
-##
-##sol_node, time_node, allocation_node, gc_node, memory_counters_node = @timed solve(prob_node, RK4(), u0 = stack(ustart), p = θ_node, saveat = saveat, dt=dt);
 
 
 
@@ -256,57 +270,18 @@ p5=Plots.heatmap(title="u_INS-u_CNODE",state.u[1] - sol_node.u[end][:, :, 1])
 p6=Plots.heatmap(title="u_CNODE-u_ODE",sol_node.u[end][:, :, 1] - sol_ode.u[end][:, :, 1])
 Plots.plot(p1, p2, p3, p4,p5,p6, layout=(2,3), size=(900,600))
 
-# and plot the vorticty as well
-vins = INS.vorticity((state.u[1], state.u[2]), setup)
-vode = INS.vorticity((sol_ode.u[end][:, :, 1], sol_ode.u[end][:, :, 2]), setup) 
-vnode = INS.vorticity((sol_node.u[end][:, :, 1], sol_node.u[end][:, :, 2]), setup) 
 
-Plots.heatmap(title="vorticity INS-ODE",  - )
-p1=Plots.heatmap(title="vorticity in SciML ODE",vode)
-p2=Plots.heatmap(title="vorticity in SciML CNODE",vnode)
-p3=Plots.heatmap(title="vorticity in INS",vins)
-# and compare them
-p4=Plots.heatmap(title="INS-ODE",vins-vode)
-p5=Plots.heatmap(title="INS-CNODE",vins-vnode)
-p6=Plots.heatmap(title="CNODE-ODE",vode-vnode)
-Plots.plot(p1, p2, p3, p4,p5,p6, layout=(2,3), size=(900,600))
-
-
-
-### Animate solution using Makie
-### ! we can plot either the vorticity or the velocity field, however notice that the vorticity is 'flashing' (unstable)
-##using GLMakie
-##let
-##    (; Iu) = setup.grid
-##    i = 1
-###    obs = Observable(sol.u[1][Iu[i], i])
-###    ω = IncompressibleNavierStokes.vorticity((u[:, :, 1], u[:, :, 2]), setup)
-##    obs = Observable(INS.vorticity((sol_ode.u[1][:, :, 1], sol_ode.u[1][:, :, 2]), setup))
-##    fig = GLMakie.heatmap(obs)
-##    fig |> display
-##    for u in sol_ode.u
-###        obs[] = u[Iu[i], i]
-##        obs[] = INS.vorticity((u[:, :, 1], u[:, :, 2]), setup)
-##        # fig |> display
-##        sleep(0.05)
-##    end
-##end
-##
-### plots using Plots.jl
-##using Plots, Printf
-##anim = Animation()
-##for (idx, (t, u)) in enumerate(zip(sol_node.t, sol_node.u))
-##    ω = INS.vorticity((u[:, :, 1], u[:, :, 2]), setup)
-##    title = @sprintf("Vorticity, t = %.3f", t)
-##    fig = Plots.heatmap(ω'; xlabel = "x", ylabel = "y", title)
-##    frame(anim, fig)
-##end
-##gif(anim, fps = 15)
-
-
+# Compute the divergence of the final state
+div_INS = INS.divergence(state.u, setup)
+div_ode = INS.divergence((sol_ode.u[end][:,:,1],sol_ode.u[end][:,:,2]), setup)
+div_node = INS.divergence((sol_node.u[end][:,:,1],sol_node.u[end][:,:,2]), setup)
+p1 = Plots.heatmap(title="div_INS",div_INS)
+p2 = Plots.heatmap(title="div_ODE",div_ode)
+p3 = Plots.heatmap(title="div_NODE",div_node)
+Plots.plot(p1, p2, p3, layout=(1,3), size=(900,300))
 
 ########################
-# Test the training
+# Test the autodiff using Enzyme 
 using Enzyme
 using ComponentArrays
 using SciMLSensitivity
@@ -316,23 +291,24 @@ ode_data = Array(sol_ode)
 
 
 # First test Enzyme for a one step a priori loss
-#dual_P1 = (cache_F, cache_div, cache_p, psolver)
-const U = stack(state.u) 
-#dual_temp = copy(temp)
+U = stack(state.u) 
 function fen(θ, u0, P1, temp)
-    return sum(U .- dudt_out(temp, u0, (P1,θ), 0.0f0))
+    return sum(U .- dudt_nn(temp, u0, (P1,θ), 0.0f0))
 end
 # Some tests of Enzyme
-#dtheta = similar(pinit)
 dtheta = Enzyme.make_zero(pinit)
-#du =ones(Float32, size(u0stacked))
+u0stacked = stack(ustart)
 du = Enzyme.make_zero(u0stacked)
+P1
 dual_P1 = Enzyme.make_zero(P1)
 dual_temp = Enzyme.make_zero(temp)
 # Compute the autodiff using Enzyme
 Enzyme.autodiff(Enzyme.Reverse, fen, Active, Duplicated(pinit, dtheta), Duplicated(u0stacked, du), Duplicated(P1, dual_P1), Duplicated(temp, dual_temp))
+dtheta
 # this shows us that Enzyme can differentiate our force. But what about SciML solvers?
+show(err)
 
+##33 from here  [..]
 
 # Then I can test Enzyme for a posteriori loss 
 # pre-allcoate
