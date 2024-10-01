57bacfd39443523064d6f98a600fe499a575df88
Luisa Orozco <l.orozco@esciencecenter.nl>
2024-10-01 10:25:29 +0200

    Remove Burgers and KdV things

--- src/CoupledNODE.jl
+++ src/CoupledNODE.jl
@@ -5,13 +5,13 @@ ArrayType = CUDA.functional() ? CUDA.CuArray : Array
 
 include("utils.jl")
 include("train.jl")
+
 include("models/FNO.jl")
 include("models/cnn.jl")
+
 include("loss/loss_priori.jl")
 include("loss/loss_posteriori.jl")
 
-# Modules for the examples
-include("equations/Burgers.jl")
 include("equations/NavierStokes_utils.jl")
 
 end # module CoupledNODE
