58c5546533b9c5975e037cc601364cf7b9357927
Victor Azizi <victor@lipsum.eu>
2024-11-21 07:54:47 +0100

    upgrade to julia 1.11 (incompatible with 1.10)

--- simulations/NavierStokes_2D/scripts/generate_data.jl
+++ simulations/NavierStokes_2D/scripts/generate_data.jl
@@ -6,9 +6,6 @@ T = Float32
 rng = Random.Xoshiro(123)
 
 # Generate the data using NeuralClosure
-# Add NeuralClosure from the git repo, until it is added to the public repo
-using Pkg
-Pkg.add(url = "git@github.com:DEEPDIP-project/NeuralClosure.jl.git")
 using NeuralClosure: NeuralClosure as NC
 
 # Number of simulations to generate for each grid
