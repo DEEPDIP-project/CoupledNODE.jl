e7cbc28fdd5376c470cbe05a2a7db6700304e457
SCiarella <simoneciarella@gmail.com>
2025-02-07 10:49:45 +0100

    autopush

--- test/test_gpu_aposteriori.jl
+++ test/test_gpu_aposteriori.jl
@@ -77,7 +77,7 @@ using OptimizationOptimisers: OptimizationOptimisers
         rng = rng,
         #device = device
     )
-    θ = device(θ)
+    θ = θ |> device
     #@test is_on_gpu(θ.layer_4.weight) # Check that the parameters are on the GPU
     @warn "*** -> typeof(θ): $(typeof(θ))"
     #st = device(st)
