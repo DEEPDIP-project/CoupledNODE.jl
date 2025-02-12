3a9ef42f99768a07f7aa309e0bed7f86af99723e
SCiarella <simoneciarella@gmail.com>
2025-02-12 15:43:50 +0100

    autopush

--- src/checkpoints.jl
+++ src/checkpoints.jl
@@ -25,7 +25,9 @@ function load_checkpoint(checkfile)
     callbackstate = checkpoint.callbackstate
     trainstate = checkpoint.trainstate
     epochs_trained = length(callbackstate.lhist_train)
-    dev = CUDA.functional() ? Lux.gpu_device() : Lux.cpu_device()
-    @info "Loading checkpoint from $checkfile and putting it on $dev.\nPrevious training reached epoch $(epochs_trained)."
-    return callbackstate |> dev, trainstate |> dev, epochs_trained
+    #dev = CUDA.functional() ? Lux.gpu_device() : Lux.cpu_device()
+    #@info "Loading checkpoint from $checkfile and putting it on $dev.\nPrevious training reached epoch $(epochs_trained)."
+    #return callbackstate |> dev, trainstate |> dev, epochs_trained
+    @info "Loading checkpoint from $checkfile.\nPrevious training reached epoch $(epochs_trained)."
+    return callbackstate, trainstate, epochs_trained
 end
